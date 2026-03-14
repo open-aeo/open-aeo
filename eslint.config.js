6c17f5305e779f409572123709281ae766d71d6a
brgv-code <bhargav.virupakshi@gmail.com>
2026-03-14 14:13:53 +0100

    chore: update ESLint configuration and dependencies to improve linting support

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
+// @ts-check
 import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
+export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     ignores: ["dist/**", "node_modules/**"],
   },
